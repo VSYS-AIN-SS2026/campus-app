83c99e822c425dea2c16c1236c148ab3bc650068
Benno Hahn <65913048+BennoHahn@users.noreply.github.com>
2026-04-21 19:21:59 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL=https://yemmuitnxoyhxdsbfcfb.supabase.co
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_wU2P-p7BOib7lfTnuWjFSQ_7nekKIa4
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
